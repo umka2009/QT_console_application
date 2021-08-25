ae0783eb1f9fa964ad8de4d927f4f44bab74bc46
umka2009 <uyatsishin@gmail.com>
2021-08-25 09:38:18 +0300

    adit file , 'toml'

--- QtProjConsole/main.cpp
+++ QtProjConsole/main.cpp
@@ -80,7 +80,7 @@ void GetCommandLine(const QCoreApplication& app, QCommandLineParser& parser)
     SetOptionsMultiple(parser);
 
     parser.addPositionalArgument("-s, --source", QCoreApplication::translate("main",
-        "Source file to read \"source.toml\" | \"source.toml11\"."), "[-p] [-d] | ([-s] | [--source]) <source> ");
+        "Source file to read \"source.toml\" | \"source.toml\"."), "[-p] [-d] | ([-s] | [--source]) <source> ");
 
     parser.process(app);
 }
@@ -94,15 +94,15 @@ void SetOptionsSingle(QCommandLineParser& parser)
 void SetOptionsMultiple(QCommandLineParser& parser)
 {
     parser.addOption(QCommandLineOption(QStringList() << "d" << "default",
-        QCoreApplication::translate("main", "Default source file to read \"source.toml\" | \"source.toml11\"")));
+        QCoreApplication::translate("main", "Default source file to read \"source.toml\" | \"source.toml\"")));
     parser.addOption(QCommandLineOption(QStringList() << "s" << "source",
-        QCoreApplication::translate("main", "Source file to read [FilePath...\\NameSource.(toml | toml11)].")));
+        QCoreApplication::translate("main", "Source file to read [FilePath...\\NameSource.(toml | toml)].")));
 }
 QString GetDescription()
 {
     QString description;
     description += "\nConsole application that accepts a configuration file as a parameter.\n";
     description += "The file from which the application should read the parameter list\n";
-    description += "must be in .toml , .toml11 format<br> (Filename extension - .toml , .toml11)";
+    description += "must be in .toml , .toml format<br> (Filename extension - .toml , .toml)";
     return description;
 }
