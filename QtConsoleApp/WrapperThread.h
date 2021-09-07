1320e4fb6080fe72acefc0ba86825ea8e6c15c74
umka2009 <uyatsishin@gmail.com>
2021-09-07 05:49:07 +0300

    add WrapperThread.h : constructors, operators and methods

--- QtConsoleApp/WrapperThread.h
+++ QtConsoleApp/WrapperThread.h
@@ -3,11 +3,23 @@
 #ifndef WRAPPERTHREAD_H
 #define	WRAPPERTHREAD_H
 
+#include <vector>
+#include <thread>
+
 class WrapperThread
 {
 public:
+	WrapperThread();
+	~WrapperThread();
+	void Emplace_back(std::thread&& thread_);
+	int Size() const;
+	const std::thread& operator[] (size_t it) const;
+
+	WrapperThread(const WrapperThread& tmpThread) = delete;
+	WrapperThread& operator=(const WrapperThread& tmpThread) = delete;
 
 private:
+	std::vector<std::thread> myThread;
 };
 
 #endif // !WRAPPERTHREAD_H
