d613c89bef64531fd934de778321e1e9cc391106
umka2009 <uyatsishin@gmail.com>
2021-09-07 20:42:10 +0300

    add ThreadsReadFile and Sum

--- QtConsoleApp/Worker.cpp
+++ QtConsoleApp/Worker.cpp
@@ -55,7 +55,65 @@ void Worker::ThreadFunction() {
 
 };
 
-void ThreadsReadFile()
+void Worker::ThreadsReadFile(int sizeBuf, int numThreads)
 {
+    try
+    {
+        
+        WrapperThread paralelRead;
+        std::recursive_mutex lock;
+        //std::cout << "threadId : " << std::this_thread::get_id() << std::endl;
+        std::uintmax_t size = fs::file_size(filePath) / 2;
+        std::uintmax_t sizeBufTemp = (size / numThreads <= sizeBuf) ? sizeBuf : size / numThreads ;
+        std::ifstream fileStream(filePath, std::ios::binary | std::fstream::in);
 
+        if (fileStream.is_open())
+        {
+            std::string temp(sizeBufTemp, '\0');
+            while (fileStream && !fileStream.eof())
+            {
+                fileStream.getline(&temp[0], sizeBufTemp, '\0');
+                paralelRead.Emplace_back(std::thread(&Worker::Sum, this, temp, std::ref(lock)));
+            }
+        }
+        else
+            throw std::runtime_error("Can not open file " + filePath);
+    }
+    catch (const std::exception& err)
+    {
+        qCritical() << "Error exception : " << err.what();
+    }
+    catch (...)
+    {
+        qCritical() << "Uncaught error exception!";
+    }
+ };
+
+void Worker::Sum(std::string_view buf, std::recursive_mutex& lock)
+{
+    int temp = 0;
+    auto tempIterEnd = buf.end();
+    auto tempIterBegin = buf.begin();
+
+    if (action == "+")
+    {
+        auto findSpace = [](auto val)
+        {
+            return val == ' ';
+        };
+        for (auto it = std::find_if(tempIterBegin, tempIterEnd, findSpace); it != tempIterEnd; it = (std::find_if(tempIterBegin, tempIterEnd, findSpace)))
+        {
+            std::string tempBuf(tempIterBegin, it);
+            std::regex reg("^[0-9]{1,10}$");
+            if (std::regex_match(tempBuf, reg))
+                temp += std::stoi(tempBuf);
+            tempIterBegin = it;
+            tempIterBegin++;
+            std::this_thread::sleep_for(std::chrono::seconds(pause));
+        }
+    }
+    else
+        throw std::runtime_error("Can not [operators] action : " + action + " file :" + filePath);
+    std::unique_lock<std::recursive_mutex > locker(lock);
+    rez += temp;
 };
--- QtConsoleApp/Worker.h
+++ QtConsoleApp/Worker.h
@@ -11,6 +11,11 @@
 #include <chrono>
 #include <mutex>
 #include <QtCore/QCoreApplication>
+#include <filesystem>
+#include <regex>
+#include "WrapperThread.h"
+
+namespace fs = std::filesystem;
 
 class Worker
 {
@@ -21,13 +26,16 @@ public:
 
 	// parallel execution
 	void ThreadFunction();
-	void ThreadsReadFile();
+	void ThreadsReadFile(int sizeBuf, int numThreads);
+	void Sum(std::string_view buf, std::recursive_mutex& lock);  // std::string_view
 	int Getrez() const { return rez; };
+	// std::recursive_mutex lock;
 private:
 	int pause;
 	int rez = 0;
 	std::string action;
 	std::string filePath;
+
 };
 
 #endif // !WORKER_H
